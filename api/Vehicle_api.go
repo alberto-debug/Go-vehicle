7348ec351c5e9e63ee74d03db176f0f3ab51a6af
Alberto <albertojunior848423803@gmail.com>
2025-05-11 16:53:56 +0300

    changes

--- api/Vehicle_api.go
+++ api/Vehicle_api.go
@@ -1,8 +1,14 @@
 package api
 
+import "fmt"
+
 type ApiVehicle struct {
 	Marca      string `json:"marca"`
 	Modelo     string `json:"modelo"`
 	Chassi     string `json:"chassi"`
 	Licenciado bool   `json:"Licenciado"`
+}
+
+func GetVehicleData(placa string) (ApiVehicle, error) {
+	url := fmt.Sprint("https://my.api.mockaroo.com/veiculos?key=55ad1cd0&placa=%s", placa)
 }
