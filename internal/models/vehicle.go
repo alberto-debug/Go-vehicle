6ea026c6b35d8ddae3746756a481dcef89551b56
Alberto <albertojunior848423803@gmail.com>
2025-05-11 16:03:15 +0300

    changes

--- internal/models/vehicle.go
+++ internal/models/vehicle.go
@@ -1,1 +1,12 @@
 package models
+
+type Vehicle struct {
+	ID           int    `json:"id"`
+	Proprietario string `json:"proprietario"`
+	CPF          string `json:"cpf"`
+	Placa        string `json:"placa"`
+	Marca        string `json:"marca"`
+	Modelo       string `json:"modelo"`
+	Chassi       string `json:"chassi"`
+	Licenciado   bool   `json:"licenciado"`
+}
