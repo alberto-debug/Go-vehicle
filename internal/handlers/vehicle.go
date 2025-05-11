a439904e078a60e1f22d681792c754ff6baf99f0
Alberto <albertojunior848423803@gmail.com>
2025-05-11 18:22:41 +0300

    changes

--- internal/handlers/vehicle.go
+++ internal/handlers/vehicle.go
@@ -1,3 +1,13 @@
 package handlers
 
+import (
+	"net/http"
+
+	"github.com/alberto-debug/Vehicle/internal/models"
+)
+
 // This is the HTTP handler for the /veiculos route. It uses the service layer to register the vehicle.
+
+func CreateVehicle(w http.ResponseWriter, r *http.ResponseWriter) {
+	var input models.Vehicle
+}
