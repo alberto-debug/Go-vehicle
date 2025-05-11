0c1197ab0986f2dfaf5c6f089ab021331877c4c8
Alberto <albertojunior848423803@gmail.com>
2025-05-11 19:12:23 +0300

    changes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"github.com/gorilla/mux"
+
+	"github.com/alberto-debug/Vehicle/internal/handlers"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/veiculos", handlers.CreateVehicle).Methods("POST")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/alberto-debug/Vehicle
 
 go 1.24.1
+
+require github.com/gorilla/mux v1.8.1
