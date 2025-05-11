43663fb318e03a7208fd250ce5618acd859b05e9
Alberto <albertojunior848423803@gmail.com>
2025-05-11 19:15:04 +0300

    changes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
 
 	"github.com/alberto-debug/Vehicle/internal/handlers"
@@ -9,4 +12,9 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/veiculos", handlers.CreateVehicle).Methods("POST")
+
+	log.Println("Server in running on http://8080")
+	if err := http.ListenAndServe(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
