64ab97197a20088ecbfbe27c43beab786d60789c
Alberto <albertojunior848423803@gmail.com>
2025-05-11 17:48:54 +0300

    changes

--- internal/services/vehicle_service.go
+++ internal/services/vehicle_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"github.com/alberto-debug/Vehicle/api"
 	"github.com/alberto-debug/Vehicle/internal/models"
 	"github.com/alberto-debug/Vehicle/internal/storage"
 )
@@ -8,7 +9,15 @@ import (
 // This function orchestrates the vehicle registration process.
 
 func RegisterVehicle(input models.Vehicle) (models.Vehicle, error) {
+	// Check CPF and Placa uniqueness
 	if err := storage.ValidateVehicle(input.CPF, input.Placa); err != nil {
+		return models.Vehicle{}, err
+	}
+
+	// Call external API
+
+	apiData, err := api.GetVehicleData(input.Placa)
+	if err != nil {
 		return models.Vehicle{}, err
 	}
 }
